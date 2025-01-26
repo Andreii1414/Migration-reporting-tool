682dbcd044382e1de515ead80d6fc46ca8df9a15
Andreii1414 <95772732+Andreii1414@users.noreply.github.com>
2025-01-26 23:01:23 +0200

    Update turtleConverter.js

--- backend/src/utils/turtleConverter.js
+++ backend/src/utils/turtleConverter.js
@@ -3,7 +3,14 @@ function sanitizeTitle(title) {
 
     return title.replace(/[\"'\\\/<>|:*?]/g, "").trim();
 }
+function sanitizeText(input) {
+    let sanitized = input.replace(/[\r\n\t]+/g, ' ') 
+                        .replace(/\s+/g, ' ')        
+                        .trim();                     
 
+    sanitized = sanitized.replace(/[\\/<>"'`{}\[\]()*&^%$#@!+=|;:]/g, '');
+    return sanitized;
+}
 function convertToTurtle(reports) {
     let turtleData = `
         @prefix ex: <http://example.org/> .
@@ -12,12 +19,14 @@ function convertToTurtle(reports) {
 
     reports.forEach(report => {
         const sanitizedTitle = sanitizeTitle(report.title);
+        const sanitizedDescription = sanitizeText(report.description);
         turtleData += `
             ex:report${report._id} a ex:Report ;
                 ex:title "${sanitizedTitle}" ;
                 ex:date "${report.date.toISOString()}"^^xsd:dateTime ;
                 ex:species ex:species${report.speciesId._id} ;
                 ex:speciesName "${report.speciesId.name}";
+                ex:description "${sanitizedDescription}" ;
                 ex:imageUrl "${report.imageUrl}" ;
                 ex:latitude "${report.latitude}"^^xsd:float ;
                 ex:longitude "${report.longitude}"^^xsd:float ;
